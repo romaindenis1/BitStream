cebffb8eea8ac0fd7c3e343d52966c4a2a5b22a0
Romain <163823947+romaindenis1@users.noreply.github.com>
2025-11-07 11:23:53 +0100

    create(protocol) : discussed and created protocol v1 (credit to ASETML)
    [15mins][WIP]
    
    - Protocol still has things that need to be done, the final implementation will differ

---
+++ src/ISong.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitRuisseau
+{
+    public interface ISong
+    {
+        /// <summary>
+        /// The song title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The song artist
+        /// </summary>
+        public string Artist { get; set; }
+
+        /// <summary>
+        /// The song release date
+        /// </summary>
+        public int Year { get; set; }
+
+        /// <summary>
+        /// The song duration in milliseconde
+        /// </summary>
+        public TimeSpan Duration { get; set; }
+
+        /// <summary>
+        /// The song file size in bytes
+        /// </summary>
+        public int Size { get; set; }
+
+        /// <summary>
+        /// The song featuring artists
+        /// </summary>
+        public string[] Featuring { get; set; }
+    }
+}
